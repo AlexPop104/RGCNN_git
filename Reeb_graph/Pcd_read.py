eb247ce934ae76cb8bc94e3cab32cf80d9ff8e43
AlexPop104 <alexandrupop104@gmail.com>
2022-02-03 13:17:20 +0200

    Added functional Reeb graph

---
+++ Reeb_graph/Pcd_read.py
@@ -0,0 +1,5 @@
+import open3d as o3d
+import numpy as np
+
+
+pcd_load= o3d.io.read_point_cloud("/home/alex-pop/Desktop/Doctorat/Side_projects/trai/trai/PC-NBV/Alex_pop_work/build/Test_folder/chair_0983.pcd")
