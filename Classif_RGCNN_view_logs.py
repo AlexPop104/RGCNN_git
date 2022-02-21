d12ec7fb513c62f5af9c4f3358cfe674165db138
AlexPop104 <alexandrupop104@gmail.com>
2022-02-21 11:37:27 +0200

    RGCNN Classif 3 Layers functional

--- Classif_RGCNN_view_logs.py
+++ Classif_RGCNN_view_logs.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 num_modelnet=40
 
 
-all_losses=np.load('/home/alex/Alex_documents/RGCNN_git/data/losses_2.npy' )
+all_losses=np.load('/home/alex/Alex_documents/RGCNN_git/data/logs/Network_performances/Type_20_02_22_22:47:46_losses.npy' )
 plt.plot(all_losses, '-b', label='Training loss')
 plt.title('Training loss', fontdict=None, loc='center', pad=None)
 plt.show()
 
-test_accuracy=np.load('/home/alex/Alex_documents/RGCNN_git/data/test_accuracy_2.npy')
+
+
+test_accuracy=np.load('/home/alex/Alex_documents/RGCNN_git/data/logs/Network_performances/Type_20_02_22_22:47:46test_accuracy.npy')
 plt.plot(test_accuracy, '-r', label='Test accuracy')
 plt.title('Test accuracy', fontdict=None, loc='center', pad=None)
 plt.show()
@@ -21,7 +23,7 @@ a=all_losses.shape
 nr_epochs=a[0]
 
 
-confusion_matrix_collection=np.load('/home/alex/Alex_documents/RGCNN_git/data/conf_matrix_2.npy' )
+confusion_matrix_collection=np.load('/home/alex/Alex_documents/RGCNN_git/data/logs/Network_performances/Type_20_02_22_22:47:46_conf_matrix.npy' )
 
 final_matrix=(confusion_matrix_collection[1+(nr_epochs-1)*num_modelnet:1+(nr_epochs)*num_modelnet])
 
