0b70687b5e283b2918af4d84459ca9f3a05bd47b
AlexPop104 <alexandrupop104@gmail.com>
2022-03-03 13:51:09 +0200

    Created function for FPS using nearest, must start GCN architecture

--- Classif_RGCNN_functional_FPS.py
+++ Classif_RGCNN_functional_FPS.py
@@ -1,12 +1,10 @@
 import time
-import torch
 
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
 
 from torch import nn
-
-
+import torch
 from torch.nn import Parameter
 
 from torch_geometric.nn import fps,radius_graph,nearest
@@ -21,9 +19,8 @@ from torch_geometric.nn.inits import zeros
 from torch_geometric.typing import OptTensor
 from torch_geometric.utils import (add_self_loops, get_laplacian,
                                    remove_self_loops)
-
-import torch_geometric as tg
 import torch as t
+import torch_geometric as tg
 
 from torch_geometric.utils import get_laplacian as get_laplacian_pyg
 from torch_geometric.transforms import Compose
@@ -64,7 +61,7 @@ class cls_model(nn.Module):
         # self.get_laplacian = GetLaplacian(normalize=True)
         self.relu1 = nn.ReLU()
         self.relu2 = nn.ReLU()
-        self.relu3 = nn.ReLU()
+        self.relu_Reeb = nn.ReLU()
         self.relu4 = nn.ReLU()
         self.relu5 = nn.ReLU()
 
@@ -72,9 +69,9 @@ class cls_model(nn.Module):
 
         self.conv1 = conv.DenseChebConv(6, 128, 6)
         self.conv2 = conv.DenseChebConv(128, 512, 5)
-        #self.conv3 = conv.DenseChebConv(512, 1024, 3)
+        self.conv_Reeb = conv.DenseChebConv(128, 512, 5)
         
-        #self.fc1 = nn.Linear(1024, 512, bias=True)
+        self.fc1 = nn.Linear(1024, 512, bias=True)
         self.fc2 = nn.Linear(512, 128, bias=True)
         self.fc3 = nn.Linear(128, class_num, bias=True)
         
@@ -89,11 +86,11 @@ class cls_model(nn.Module):
         self.regularization = []
 
 
-    def forward(self, x):
+    def forward(self, x,k,batch_size,num_points,sccs):
         self.regularizers = []
         with torch.no_grad():
             L = conv.pairwise_distance(x) # W - weight matrix
-                
+            L = conv.get_one_matrix_knn(L, k,batch_size,num_points)
             L = conv.get_laplacian(L)
 
         out = self.conv1(x, L)
@@ -103,36 +100,55 @@ class cls_model(nn.Module):
             self.regularizers.append(t.linalg.norm(t.matmul(t.matmul(t.permute(out, (0, 2, 1)), L), out))**2)
         
         if self.one_layer == False:
+
+
             with torch.no_grad():
+                Vertices_final_FPS=torch.zeros([batch_size,sccs.shape[1], out.shape[2]], dtype=torch.float32,device='cuda')
+
+                for batch_iter in range(batch_size):   
+                    for i in range(sccs.shape[1]):
+                        Vertices_pool_FPS=torch.zeros([sccs[batch_iter,i].shape[0],out.shape[2]], dtype=torch.float32,device='cuda')
+                        
+                        Vertices_pool_FPS=out[batch_iter,sccs[batch_iter,i]]
+
+                        Vertices_final_FPS[batch_iter,i],_ =t.max(Vertices_pool_FPS, 0)
+
+
                 L = conv.pairwise_distance(out) # W - weight matrix
+                L = conv.get_one_matrix_knn(L, k,batch_size,num_points)
                 L = conv.get_laplacian(L)
-            
+
+
             out = self.conv2(out, L)
             out = self.relu2(out)
             if self.reg_prior:
                 self.regularizers.append(t.linalg.norm(t.matmul(t.matmul(t.permute(out, (0, 2, 1)), L), out))**2)
-    
+
             with torch.no_grad():
-                L = conv.pairwise_distance(out) # W - weight matrix
+                L = conv.pairwise_distance(Vertices_final_FPS) # W - weight matrix
+                L = conv.get_one_matrix_knn(L, 40,batch_size,num_points)
                 L = conv.get_laplacian(L)
-            
-            # out = self.conv3(out, L)
-            # out = self.relu3(out)
-            
-            # if self.reg_prior:
-            #     self.regularizers.append(t.linalg.norm(t.matmul(t.matmul(t.permute(out, (0, 2, 1)), L), out))**2)
+
+            out_Reeb=self.conv_Reeb(Vertices_final_FPS,L)
+            out_Reeb=self.relu_Reeb(out_Reeb)
+    
+            if self.reg_prior:
+                self.regularizers.append(t.linalg.norm(t.matmul(t.matmul(t.permute(out_Reeb, (0, 2, 1)), laplacian_Reeb_final), out_Reeb))**2)
     
             out, _ = t.max(out, 1)
+            out_Reeb, _ = t.max(out_Reeb, 1)
+
+            out=torch.cat((out_Reeb,out),1)
 
             # ~~~~ Fully Connected ~~~~
             
-            # out = self.fc1(out)
+            out = self.fc1(out)
 
-            # if self.reg_prior:
-            #     self.regularizers.append(t.linalg.norm(self.fc1.weight.data[0]) ** 2)
-            #     self.regularizers.append(t.linalg.norm(self.fc1.bias.data[0]) ** 2)
+            if self.reg_prior:
+                self.regularizers.append(t.linalg.norm(self.fc1.weight.data[0]) ** 2)
+                self.regularizers.append(t.linalg.norm(self.fc1.bias.data[0]) ** 2)
 
-            # out = self.relu4(out)
+            out = self.relu4(out)
             #out = self.dropout(out)
 
             out = self.fc2(out)
@@ -154,84 +170,36 @@ class cls_model(nn.Module):
 
 criterion = torch.nn.CrossEntropyLoss()  # Define loss criterion.
 
-def train(model, optimizer, loader, regularization):
+def train(model, optimizer, loader,all_sccs,k,num_points, regularization):
     model.train()
     total_loss = 0
     for i, data in enumerate(loader):
         optimizer.zero_grad()
-        
+
+
         x=data.pos
 
         nr_points_fps=55
-        nr_points_batch=int(data.batch.shape[0]/data.batch.unique().shape[0])
-
-        
-        index = fps(x, data.batch, ratio=float(nr_points_fps*data.batch.unique().shape[0]/data.pos.shape[0]) , random_start=True)
-
-        fps_x=x[index]
-        fps_batch=data.batch[index]
-
-        cluster = nearest(x, fps_x, data.batch, fps_batch)
 
 
-        batch_correction=torch.arange(0,data.batch.unique().shape[0],device='cpu')
-        batch_correction=torch.reshape(batch_correction,[data.batch.unique().shape[0],1])
-        batch_correction=torch.tile(batch_correction,(1,nr_points_batch))
-        batch_correction=torch.reshape(batch_correction,[data.batch.unique().shape[0]*nr_points_batch])
-
-        Batch_indexes=batch_correction
-
-        batch_correction_num_points=batch_correction*nr_points_batch
-        batch_correction=batch_correction*nr_points_fps
-        
-
-        
+        Matrix_numpy=conv.get_fps_matrix(x,data,55)
 
 
-        cluster_new=torch.subtract(cluster,batch_correction)
-        #cluster_new=torch.add(cluster_new,batch_correction_num_points)
 
-        edge_index_1=torch.arange(0,data.batch.unique().shape[0]*nr_points_batch,device='cpu')
-        edge_index_2=cluster_new
-
-        edge_index_final=torch.cat((torch.unsqueeze(edge_index_1,1),torch.unsqueeze(edge_index_2,1)),axis=1)
-        edge_index_final=torch.transpose(edge_index_final,0,1)
-        
-        edge_weight=torch.ones([data.batch.unique().shape[0]*nr_points_batch])
-
-
-        Matrix_near=tg.utils.to_dense_adj(edge_index_final,Batch_indexes,edge_weight)
-
-
-        Matrix_near=Matrix_near[:,:,0:nr_points_fps]
-
-        Matrix_near= Matrix_near.permute(0, 2, 1)
-
-        Matrix_near_2,Matrix_near_indices= torch.sort(Matrix_near,dim=2,descending=True)
-
-        Matrix_near_3=torch.multiply(Matrix_near_2,Matrix_near_indices)
-        Matrix_near_3,_=torch.sort(Matrix_near_3,dim=2,descending=True)
-        Matrix_near_4=torch.reshape(Matrix_near_3,(data.batch.unique().shape[0]*nr_points_fps,nr_points_batch))
-
-        Matrix_numpy=Matrix_near_4.cpu().detach().numpy()
-
-       
-
-        for i in range(nr_points_fps*data.batch.unique().shape[0]):
-            Matrix_numpy[i]=np.where(Matrix_numpy[i]==0,Matrix_numpy[i][0],Matrix_numpy[i])
-
-        
-        Matrix_numpy_2=conv.get_fps_matrix(x,data,55)
-
-        
 
         x = torch.cat([data.pos, data.normal], dim=1)
         x = x.reshape(data.batch.unique().shape[0], num_points, 6)
 
-    
+
+        sccs_batch=Matrix_numpy
+        
+
+        sccs_batch=sccs_batch.astype(int)
+
+        sccs_batch=np.reshape(sccs_batch,(data.batch.unique().shape[0],nr_points_fps,all_sccs.shape[1]))
 
 
-        logits, regularizers  = model(x.to(device))
+        logits, regularizers  = model(x.to(device),k=k,batch_size=data.batch.unique().shape[0],num_points=num_points,sccs=sccs_batch)
         loss    = criterion(logits, data.y.to(device))
         s = t.sum(t.as_tensor(regularizers))
         loss = loss + regularization * s
@@ -244,28 +212,44 @@ def train(model, optimizer, loader, regularization):
     return total_loss / len(loader.dataset)
 
 @torch.no_grad()
-def test(model, loader):
+def test(model, loader,all_sccs,all_Reeb_laplacian,k,num_points):
     model.eval()
 
     total_correct = 0
-    for data in loader:
+    for i,data in enumerate(loader):
         x = torch.cat([data.pos, data.normal], dim=1)
         x = x.reshape(data.batch.unique().shape[0], num_points, 6)
-        logits, _ = model(x.to(device))
+
+        sccs_batch=all_sccs[i*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1]:(i+1)*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1],0:all_sccs.shape[1]]
+        reeb_laplace_batch=all_Reeb_laplacian[i*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1]:(i+1)*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1],0:all_Reeb_laplacian.shape[1]]
+
+        
+        sccs_batch=np.reshape(sccs_batch,(data.batch.unique().shape[0],all_Reeb_laplacian.shape[1],all_sccs.shape[1]))
+        reeb_laplace_batch=np.reshape(reeb_laplace_batch,(data.batch.unique().shape[0],all_Reeb_laplacian.shape[1],all_Reeb_laplacian.shape[1]))
+
+        logits, _ = model(x.to(device),k=k,batch_size=data.batch.unique().shape[0],num_points=num_points,laplacian_Reeb=reeb_laplace_batch,sccs=sccs_batch)
         pred = logits.argmax(dim=-1)
         total_correct += int((pred == data.y.to(device)).sum())
 
     return total_correct / len(loader.dataset)
 
-def createConfusionMatrix(model,loader):
+def createConfusionMatrix(model,loader,all_sccs,all_Reeb_laplacian,k,num_points):
     y_pred = [] # save predction
     y_true = [] # save ground truth
 
     # iterate over data
-    for  data in loader:
+    for  i,data in enumerate(loader):
         x = torch.cat([data.pos, data.normal], dim=1)
         x = x.reshape(data.batch.unique().shape[0], num_points, 6)
-        logits, _ = model(x.to(device))
+
+        sccs_batch=all_sccs[i*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1]:(i+1)*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1],0:all_sccs.shape[1]]
+        reeb_laplace_batch=all_Reeb_laplacian[i*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1]:(i+1)*data.batch.unique().shape[0]*all_Reeb_laplacian.shape[1],0:all_Reeb_laplacian.shape[1]]
+
+        
+        sccs_batch=np.reshape(sccs_batch,(data.batch.unique().shape[0],all_Reeb_laplacian.shape[1],all_sccs.shape[1]))
+        reeb_laplace_batch=np.reshape(reeb_laplace_batch,(data.batch.unique().shape[0],all_Reeb_laplacian.shape[1],all_Reeb_laplacian.shape[1]))
+
+        logits, _ = model(x.to(device),k=k,batch_size=data.batch.unique().shape[0],num_points=num_points,laplacian_Reeb=reeb_laplace_batch,sccs=sccs_batch)
         pred = logits.argmax(dim=-1)
         
         output = pred.cpu().numpy()
@@ -295,10 +279,11 @@ if __name__ == '__main__':
     os.mkdir(path)
 
     num_points = 1024
-    batch_size = 2
-    num_epochs = 100
+    batch_size = 32
+    num_epochs = 260
     learning_rate = 1e-3
     modelnet_num = 40
+    k_KNN=30
 
     F = [128, 512, 1024]  # Outputs size of convolutional filter.
     K = [6, 5, 3]         # Polynomial orders.
@@ -336,16 +321,30 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     my_lr_scheduler = lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.95)
 
+
+    
+
+    
+
+    
+    ###############################
+
+
+    # all_sccs_test, all_reeb_laplacian_test= conv.Create_Reeb_from_Dataset_batched(loader=test_loader,sccs_path=sccs_path_test,reeb_laplacian_path=reeb_laplacian_path_test,time_execution=timp_test,knn=knn_REEB,ns=ns,tau=tau,reeb_nodes_num=reeb_nodes_num,reeb_sim_margin=reeb_sim_margin,pointNumber=pointNumber)
+    # all_sccs_train, all_reeb_laplacian_train=conv.Create_Reeb_from_Dataset_batched(loader=train_loader,sccs_path=sccs_path_train,reeb_laplacian_path=reeb_laplacian_path_train,time_execution=timp_train,knn=knn_REEB,ns=ns,tau=tau,reeb_nodes_num=reeb_nodes_num,reeb_sim_margin=reeb_sim_margin,pointNumber=pointNumber)
+
+
     regularization = 1e-9
     for epoch in range(1, num_epochs+1):
         train_start_time = time.time()
-        loss = train(model, optimizer, train_loader, regularization=regularization)
+        loss = train(model, optimizer,loader=train_loader,all_sccs=all_sccs_train,all_Reeb_laplacian=all_reeb_laplacian_train,k=k_KNN,num_points=num_points,regularization=regularization)
+        
         train_stop_time = time.time()
 
         writer.add_scalar("Loss/train", loss, epoch)
         
         test_start_time = time.time()
-        test_acc = test(model, test_loader)
+        test_acc = test(model, loader=test_loader,all_sccs=all_sccs_test,all_Reeb_laplacian=all_reeb_laplacian_test,k=k_KNN,num_points=num_points)
         test_stop_time = time.time()
 
 
@@ -355,7 +354,7 @@ if __name__ == '__main__':
         print(f'\tTrain Time: \t{train_stop_time - train_start_time} \n \
         Test Time: \t{test_stop_time - test_start_time }')
 
-        writer.add_figure("Confusion matrix", createConfusionMatrix(model,test_loader), epoch)
+        writer.add_figure("Confusion matrix", createConfusionMatrix(model,test_loader,all_sccs=all_sccs_test,all_Reeb_laplacian=all_reeb_laplacian_test,k=k_KNN,num_points=num_points), epoch)
 
         if(epoch%5==0):
             torch.save(model.state_dict(), path + '/model' + str(epoch) + '.pt')
@@ -364,6 +363,56 @@ if __name__ == '__main__':
 
     
     torch.save(model.state_dict(), path + '/model' + str(epoch) + '.pt')
+
+
+       ###################################################################################################3
+    #Testing the model
+
+#     timp_train=0
+#     timp_test=0
+
+
+#     knn_REEB = 20
+#     ns = 20
+#     tau = 2
+#     reeb_nodes_num=20
+#     reeb_sim_margin=20
+#     pointNumber=200
+
+#     path_logs="/home/alex/Alex_documents/RGCNN_git/data/logs/Reeb_data/"
+#     sccs_path_test=path_logs+directory+"sccs_test.npy"
+#     reeb_laplacian_path_test=path_logs+directory+"reeb_laplacian_test.npy"
+
+#     random_rotate = Compose([
+#     RandomRotate(degrees=180, axis=0),
+#     RandomRotate(degrees=180, axis=1),
+#     RandomRotate(degrees=180, axis=2),
+# ])
+
+#     test_transform = Compose([
+#     random_rotate,
+#     SamplePoints(num_points, include_normals=True),
+#     NormalizeScale()
+# ])
+#     dataset_train = ModelNet(root=root, name=str(modelnet_num), train=True, transform=transforms)
+#     dataset_test = ModelNet(root=root, name=str(modelnet_num), train=False, transform=test_transform)
+
+#     train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, pin_memory=True)
+#     test_loader  = DataLoader(dataset_test, batch_size=batch_size)
+
+#     all_sccs_test, all_reeb_laplacian_test= conv.Create_Reeb_from_Dataset_batched(loader=test_loader,sccs_path=sccs_path_test,reeb_laplacian_path=reeb_laplacian_path_test,time_execution=timp_test,knn=knn_REEB,ns=ns,tau=tau,reeb_nodes_num=reeb_nodes_num,reeb_sim_margin=reeb_sim_margin,pointNumber=pointNumber)
+    
+    
+#     model = cls_model(num_points, F, K, M, modelnet_num, dropout=1, one_layer=False, reg_prior=True)
+#     path_saved_model="/home/alex/Alex_documents/RGCNN_git/data/logs/Trained_Models/28_02_22_21:52:37/model260.pt"
+#     model.load_state_dict(torch.load(path_saved_model))
+#     model = model.to(device)
+
+#     test_start_time = time.time()
+#     test_acc = test(model, loader=test_loader,all_sccs=all_sccs_test,all_Reeb_laplacian=all_reeb_laplacian_test,k=k_KNN,num_points=num_points)
+#     test_stop_time = time.time()
+#     print(f'Test Accuracy: {test_acc:.4f}')
+
 
     
 
