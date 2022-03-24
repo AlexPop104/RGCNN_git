2ddc7562f8423172d14af99bd6fadfe1eae9569a
DomsaVictor <domsavictor@gmail.com>
2022-03-24 11:13:14 +0200

    Classification V2

---
+++ cls_model_rambo_v2.py
@@ -0,0 +1,253 @@
+from doctest import NORMALIZE_WHITESPACE
+from site import addpackage
+import time
+
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter(log_dir='/runs/ModelNet40')
+
+from torch import nn
+import torch
+from torch.nn import Parameter
+
+from torch_geometric.datasets import ModelNet
+from torch_geometric.transforms import SamplePoints
+
+from torch_geometric.nn.inits import zeros
+from torch_geometric.typing import OptTensor
+from torch_geometric.utils import (add_self_loops, get_laplacian,
+                                   remove_self_loops)
+import torch as t
+import torch_geometric as tg
+ 
+from torch_geometric.utils import get_laplacian as get_laplacian_pyg
+from torch_geometric.transforms import Compose
+
+import ChebConv_rgcnn as conv
+import os
+from torch_geometric.transforms import NormalizeScale
+from torch_geometric.loader import DataLoader
+from datetime import datetime
+from torch.nn import MSELoss
+from torch.optim import lr_scheduler
+
+
+class cls_model(nn.Module):
+    def __init__(self, vertice ,F, K, M, class_num, regularization=0, one_layer=True, dropout=0, reg_prior:bool=True):
+        assert len(F) == len(K)
+        super(cls_model, self).__init__()
+
+        self.F = F
+        self.K = K
+        self.M = M
+
+        self.one_layer = one_layer
+
+        self.reg_prior = reg_prior
+        self.vertice = vertice
+        self.regularization = regularization    # gamma from the paper: 10^-9
+        self.dropout = dropout
+        self.regularizers = []
+
+        # self.get_laplacian = GetLaplacian(normalize=True)
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+        self.relu3 = nn.ReLU()
+        self.relu4 = nn.ReLU()
+        self.relu5 = nn.ReLU()
+
+        self.dropout = torch.nn.Dropout(p=self.dropout)
+
+        self.conv1 = conv.DenseChebConv(6, 128, 1)
+        self.conv2 = conv.DenseChebConv(128, 512, 1)
+        self.conv3 = conv.DenseChebConv(512, 1024, 1)
+
+
+        self.fc1 = nn.Linear(1024, 512, bias=True)
+        self.fc2 = nn.Linear(512, 128, bias=True)
+        self.fc3 = nn.Linear(128, class_num, bias=True)
+        
+        
+        self.fc_t = nn.Linear(128, class_num)
+
+        self.max_pool = nn.MaxPool1d(self.vertice)
+
+        if one_layer == True:
+            self.fc = nn.Linear(128, class_num)
+        
+        self.L = []
+        self.x = []
+
+    @torch.no_grad()
+    def append_regularization_terms(self, x, L):
+        if self.reg_prior:
+            self.L.append(L)
+            self.x.append(x)
+    
+
+    def reset_regularization_terms(self):
+        self.L = []
+        self.x = []
+    
+
+    def forward(self, x):
+        self.reset_regularization_terms()
+
+        with torch.no_grad():
+            L = conv.pairwise_distance(x) # W - weight matrix
+            L = conv.get_laplacian(L)
+
+        out = self.conv1(x, L)        
+        self.append_regularization_terms(out, L)
+        out = self.relu1(out)
+
+
+        if self.one_layer == False:
+            with torch.no_grad():
+                L = conv.pairwise_distance(out) # W - weight matrix
+                L = conv.get_laplacian(L)
+            
+            out = self.conv2(out, L)
+            self.append_regularization_terms(out, L)
+            out = self.relu2(out)
+
+            with torch.no_grad():
+                L = conv.pairwise_distance(out) # W - weight matrix
+                L = conv.get_laplacian(L)
+            
+            out = self.conv3(out, L)
+            self.append_regularization_terms(out, L)
+            out = self.relu3(out)
+            
+            out, _ = t.max(out, 1)
+
+            # ~~~~ Fully Connected ~~~~
+            
+            out = self.fc1(out)
+
+            out = self.relu4(out)
+            #out = self.dropout(out)
+
+            out = self.fc2(out)
+
+            out = self.relu5(out)
+            #out = self.dropout(out)
+
+            out = self.fc3(out)
+        else:
+            out, _ = t.max(out, 1)
+            out = self.fc(out)
+        
+        return out, self.x, self.L
+
+criterion = torch.nn.CrossEntropyLoss()  # Define loss criterion.
+
+def compute_loss(logits, y, x, L, s=1e-9):
+    if not logits.device == y.device:
+        y = y.to(logits.device)
+    loss = criterion(logits, y)
+    l=0
+    for i in range(len(x)):
+        l += (1/2) * t.linalg.norm(t.matmul(t.matmul(t.permute(x[i], (0, 2, 1)), L[i]), x[i]))**2
+    l = l * s
+    loss += l
+    return loss    
+
+def train(model, optimizer, loader, regularization):
+    model.train()
+    total_loss = 0
+    for i, data in enumerate(loader):
+        optimizer.zero_grad()
+        x = torch.cat([data.pos, data.normal], dim=1)
+        x = x.reshape(data.batch.unique().shape[0], num_points, 6)
+        logits, out, L = model(x.to(device))
+
+        loss = compute_loss(logits, data.y, out, L, s=regularization)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * data.num_graphs
+        #if i%100 == 0:
+            #print(f"{i}: curr loss: {loss}")
+            #$print(f"{data.y} --- {logits.argmax(dim=1)}")
+    return total_loss / len(loader.dataset)
+
+@torch.no_grad()
+def test(model, loader):
+    model.eval()
+
+    total_correct = 0
+    for data in loader:
+        x = torch.cat([data.pos, data.normal], dim=1)
+        x = x.reshape(data.batch.unique().shape[0], num_points, 6)
+        logits, _, _ = model(x.to(device))
+        pred = logits.argmax(dim=-1)
+        total_correct += int((pred == data.y.to(device)).sum())
+
+    return total_correct / len(loader.dataset)
+
+if __name__ == '__main__':
+    now = datetime.now()
+    directory = now.strftime("%d_%m_%y_%H:%M:%S")
+    parent_directory = "/home/victor/workspace/thesis_ws/github/RGCNN_git/models/ModelNet/"
+    path = os.path.join(parent_directory, directory)
+    os.mkdir(path)
+
+    num_points = 1024
+    batch_size = 2
+    num_epochs = 50
+    learning_rate = 1e-3
+    modelnet_num = 40
+
+    F = [128, 512, 1024]  # Outputs size of convolutional filter.
+    K = [6, 5, 3]         # Polynomial orders.
+    M = [512, 128, modelnet_num]
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    print(f"Training on {device}")
+
+    transforms = Compose([SamplePoints(num_points, include_normals=True), NormalizeScale()])
+
+    root = "/media/rambo/ssd2/Alex_data/RGCNN/ModelNet" + str(modelnet_num)
+    print(root)
+
+    dataset_train = ModelNet(root=root, name=str(modelnet_num), train=True, transform=transforms)
+    dataset_test = ModelNet(root=root, name=str(modelnet_num), train=False, transform=transforms)
+
+    # Verification...
+    print(f"Train dataset shape: {dataset_train}")
+    print(f"Test dataset shape:  {dataset_test}")
+
+
+    train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, pin_memory=True)
+    test_loader  = DataLoader(dataset_test, batch_size=batch_size)
+    
+    model = cls_model(num_points, F, K, M, modelnet_num, dropout=1, one_layer=False, reg_prior=True)
+    model = model.to(device)
+    
+    print(model.parameters)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    my_lr_scheduler = lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.95)
+
+    regularization = 1e-9
+    for epoch in range(1, 51):
+        train_start_time = time.time()
+        loss = train(model, optimizer, train_loader, regularization=regularization)
+        train_stop_time = time.time()
+
+        writer.add_scalar("Loss/train", loss, epoch)
+        
+        test_start_time = time.time()
+        test_acc = test(model, test_loader)
+        test_stop_time = time.time()
+
+        writer.add_scalar("Acc/test", test_acc, epoch)
+        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Test Accuracy: {test_acc:.4f}')
+        print(f'\tTrain Time: \t{train_stop_time - train_start_time} \n \
+        Test Time: \t{test_stop_time - test_start_time }')
+
+        my_lr_scheduler.step()
+         
+        # Save the model every 5 epochs
+        if epoch % 5 == 0:
+            torch.save(model.state_dict(), path + '/1024_40_chebconv_model' + str(epoch) + '.pt')
