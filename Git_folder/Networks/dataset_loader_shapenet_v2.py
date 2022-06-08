c48ca24996f1ae774d13def0ea39b8ffd98afba1
AlexPop104 <alexandrupop104@gmail.com>
2022-06-08 10:15:21 +0300

    Segmentation npy save. Need to load and colour pointcloud

--- Git_folder/Networks/dataset_loader_shapenet_v2.py
+++ Git_folder/Networks/dataset_loader_shapenet_v2.py
@@ -32,6 +32,7 @@ from utils import Sphere_Occlusion_Transform
 from torch_geometric.transforms import RandomRotate
 
 import probreg_functions as probreg_f
+import Passthrough_function as pf_f
 
 def default_transforms():
     return transforms.Compose([
@@ -171,8 +172,12 @@ class PcdDataset(Dataset):
 
                 name = splits[len(splits) - 1]
 
+                name_npy = name.replace('pcd','npy')
+
 
                 total_path = self.save_path/cat/self.folder/name
+                total_path_npy =self.save_path/cat/self.folder/name_npy
+
                 pcd_save = o3d.geometry.PointCloud()
                 pcd_save.points = o3d.utility.Vector3dVector(pointcloud.pos)
 
@@ -181,9 +186,166 @@ class PcdDataset(Dataset):
                 result = aligner.register_pcds()
 
 
-                #pcd_save.normals =  o3d.utility.Vector3dVector(pointcloud.normal)
-                #o3d.io.write_point_cloud(str(total_path), pcd_save, write_ascii=True)
-                o3d.io.write_point_cloud(str(total_path), result, write_ascii=True)
+
+
+                # cloud.paint_uniform_color([0.5, 0.5, 0.5])
+
+                cloud=result
+
+
+                labels=2*np.ones((np.asarray(cloud.points).shape[0],1))
+                
+
+
+                label_indices=range(np.asarray(cloud.points).shape[0])
+
+                label_indices=np.expand_dims(label_indices, axis=1)
+
+                labels=np.concatenate((labels,label_indices),axis=1)
+                labels=np.int_(labels)
+
+                L_1=0.40
+                l_1=0.15
+                h_1=0.05
+
+                angle_x_1=0
+                angle_y_1=10
+                angle_z_1=0
+
+                centroid_x_1=-0.2874577
+                centroid_y_1= -0.14024239+0.1
+                centroid_z_1=1.21766081-0.05
+
+                color_box_1=[0.45, 0.1, 0.9]
+                color_pass_cloud_1=[1, 0.3, 0.5]
+
+
+
+                pf_filter1=o3d.geometry.PointCloud()
+                pf_filter2=o3d.geometry.PointCloud()
+                pf_filter3=o3d.geometry.PointCloud()
+
+                box_1=o3d.geometry.PointCloud()
+                box_2=o3d.geometry.PointCloud()
+                box_3=o3d.geometry.PointCloud()
+
+                label_nr_1=0
+
+                pf_filter1,box_1,cloud,labels1,remaining_labels=pf_f.Passthrough_custom(L=L_1,
+                                                    l=l_1,
+                                                    h=h_1,
+                                                    angle_x=angle_x_1,
+                                                    angle_y=angle_y_1,
+                                                    angle_z=angle_z_1,
+                                                    centroid_x=centroid_x_1,
+                                                    centroid_y=centroid_y_1,
+                                                    centroid_z=centroid_z_1,
+                                                    color_box=color_box_1,
+                                                    color_pass_cloud=color_pass_cloud_1,
+                                                    cloud=cloud,
+                                                    labels=labels,
+                                                    label_nr=label_nr_1)
+                cloud.paint_uniform_color([0.5, 0.5, 0.5])
+
+                
+                                                    
+                # o3d.visualization.draw_geometries([cloud,box_2,pf_filter2])
+                #o3d.visualization.draw_geometries([cloud])
+
+                L_2=0.27
+                l_2=1
+                h_2=0.23
+
+                angle_x_2=5
+                angle_y_2=0
+                angle_z_2=2.7
+
+                centroid_x_2=-0.2874577+0.09
+                centroid_y_2= -0.14024239+0.02
+                centroid_z_2=1.21766081
+
+                color_box_2=[0.2, 0.2, 0.5]
+                color_pass_cloud_2=[0., 1., 1.]
+
+                label_nr_2=1
+
+                pf_filter2,box_2,cloud,labels2,remaining_labels=pf_f.Passthrough_custom(L=L_2,
+                                                    l=l_2,
+                                                    h=h_2,
+                                                    angle_x=angle_x_2,
+                                                    angle_y=angle_y_2,
+                                                    angle_z=angle_z_2,
+                                                    centroid_x=centroid_x_2,
+                                                    centroid_y=centroid_y_2,
+                                                    centroid_z=centroid_z_2,
+                                                    color_box=color_box_2,
+                                                    color_pass_cloud=color_pass_cloud_2,
+                                                    cloud=cloud,
+                                                    labels=remaining_labels,
+                                                    label_nr=label_nr_1)
+
+                cloud.paint_uniform_color([0.5, 0.5, 0.5])
+
+                
+                                                    
+                #o3d.visualization.draw_geometries([cloud,box_1,pf_filter1])
+                #o3d.visualization.draw_geometries([cloud])
+
+                L_3=0.25
+                l_3=0.27
+                h_3=0.30
+
+                angle_x_3=0
+                angle_y_3=2.55
+                angle_z_3=0
+
+                centroid_x_3=-0.2874577-0.07
+                centroid_y_3= -0.14024239+0.1
+                centroid_z_3=1.21766081
+
+
+                color_box_3=[0.2, 0.2, 0.5]
+                color_pass_cloud_3=[0., 1., 1.]
+
+                label_nr_3=1
+
+                pf_filter3,box_3,cloud,labels3,remaining_labels=pf_f.Passthrough_custom(L=L_3,
+                                                    l=l_3,
+                                                    h=h_3,
+                                                    angle_x=angle_x_3,
+                                                    angle_y=angle_y_3,
+                                                    angle_z=angle_z_3,
+                                                    centroid_x=centroid_x_3,
+                                                    centroid_y=centroid_y_3,
+                                                    centroid_z=centroid_z_3,
+                                                    color_box=color_box_3,
+                                                    color_pass_cloud=color_pass_cloud_3,
+                                                    cloud=cloud,
+                                                    labels=remaining_labels,
+                                                    label_nr=label_nr_3)
+
+                cloud.paint_uniform_color([0.5, 0.5, 0.5])
+
+                labels[labels1.T[1,:],0]=label_nr_1
+                labels[labels2.T[1,:],0]=label_nr_2
+                labels[labels3.T[1,:],0]=label_nr_3
+
+                
+                labels_final=labels[:,0]
+
+                with open(total_path_npy, 'wb') as f_npy:
+                    np.save(f_npy, labels_final)
+
+                #o3d.visualization.draw_geometries([cloud,pf_filter1,pf_filter2,pf_filter3])
+
+
+
+
+
+                pcd_save.normals =  o3d.utility.Vector3dVector(pointcloud.normal)
+                o3d.io.write_point_cloud(str(total_path), pcd_save, write_ascii=True)
+
+                #o3d.io.write_point_cloud(str(total_path), result, write_ascii=True)
         return pointcloud
 
 def process_dataset(root, save_path, transform=None, num_points=512):
@@ -235,9 +397,9 @@ if __name__ == '__main__':
     choice=int(input())
 
     num_points = 400
-    root = Path("/home/alex/Alex_documents/RGCNN_git/Git_folder/data/")
+    root = Path("/home/alex/Alex_documents/RGCNN_git/data/dataset/")
 
-    root_noise_1 = Path("/home/alex/Alex_documents/RGCNN_git/Git_folder/data2/")
+    root_noise_1 = Path("/home/alex/Alex_documents/RGCNN_git/data/dataset2/")
 
     if(choice==1):
     ####Processing the datasets
